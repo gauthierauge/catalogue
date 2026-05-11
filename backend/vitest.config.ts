9002eceb93ea01a0c87db39b28d9fcd07f85fde4
gauthierauge <gauthier.auge@2dcom.fr>
2026-05-11 17:15:27 +0200

    chore: add backend project configuration
    Package dependencies, TypeScript, NestJS, Vitest and linting configs.

---
+++ backend/vitest.config.ts
@@ -0,0 +1,17 @@
+import swc from 'unplugin-swc';
+import { defineConfig } from 'vitest/config';
+import path from 'path';
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+  test: {
+    globals: true,
+    root: './',
+    include: ['src/**/*.spec.ts'],
+  },
+  plugins: [swc.vite()],
+});

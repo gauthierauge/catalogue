7837256ed4509c42619dfc80cfcbd9fc17cf6292
hich-ham-dev <hicham.hamza.dev@gmail.com>
2026-05-12 14:45:38 +0200

    chore: update migration schema

---
+++ backend/prisma/migrations/20260512124423_init/migration.sql
@@ -0,0 +1,61 @@
+/*
+  Warnings:
+
+  - You are about to drop the `Product` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- CreateEnum
+CREATE TYPE "BookType" AS ENUM ('NOVEL', 'COMIC', 'MANGA', 'ESSAY', 'BIOGRAPHY', 'MANUAL');
+
+-- CreateEnum
+CREATE TYPE "BookGenre" AS ENUM ('SCIENCE_FICTION', 'FANTASY', 'CRIME', 'ROMANCE', 'HORROR', 'ADVENTURE', 'HISTORY', 'YOUNG_ADULT', 'CHILDREN', 'NON_FICTION');
+
+-- DropTable
+DROP TABLE "Product";
+
+-- CreateTable
+CREATE TABLE "Author" (
+    "id" SERIAL NOT NULL,
+    "lastname" TEXT NOT NULL,
+    "firstname" TEXT NOT NULL,
+
+    CONSTRAINT "Author_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Book" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "price" DOUBLE PRECISION NOT NULL,
+    "description" TEXT,
+    "publisherName" TEXT,
+    "quantity" INTEGER NOT NULL DEFAULT 0,
+    "type" "BookType" NOT NULL,
+    "genre" "BookGenre" NOT NULL,
+    "isbn" TEXT NOT NULL,
+    "publicationDate" TIMESTAMP(3),
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Book_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "_AuthorToBook" (
+    "A" INTEGER NOT NULL,
+    "B" INTEGER NOT NULL,
+
+    CONSTRAINT "_AuthorToBook_AB_pkey" PRIMARY KEY ("A","B")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Book_isbn_key" ON "Book"("isbn");
+
+-- CreateIndex
+CREATE INDEX "_AuthorToBook_B_index" ON "_AuthorToBook"("B");
+
+-- AddForeignKey
+ALTER TABLE "_AuthorToBook" ADD CONSTRAINT "_AuthorToBook_A_fkey" FOREIGN KEY ("A") REFERENCES "Author"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_AuthorToBook" ADD CONSTRAINT "_AuthorToBook_B_fkey" FOREIGN KEY ("B") REFERENCES "Book"("id") ON DELETE CASCADE ON UPDATE CASCADE;

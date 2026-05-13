88176e83c4d138cfbe882050920d35f64673d015
gauthierauge <gauthier.auge@2dcom.fr>
2026-05-13 13:16:47 +0200

    fix: revert Docker secrets and fix CORS origin, Dockerfile and build

--- docker/postgres/init/01-app-user.sh
+++ docker/postgres/init/01-app-user.sh
@@ -2,8 +2,6 @@
 
 set -e
 
-APP_USER_PASSWORD=$(cat /run/secrets/app_user_password)
-
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
   CREATE USER app_user WITH PASSWORD '$APP_USER_PASSWORD';
   GRANT CONNECT ON DATABASE $POSTGRES_DB TO app_user;
